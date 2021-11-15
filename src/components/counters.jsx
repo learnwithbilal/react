af1dda4137b3749bfe303263c6674005db355d4d
learnwithbilal <learn.with.bilal@gmail.com>
2021-11-15 12:14:40 +0200

    add reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -19,9 +19,19 @@ class Counters extends React.Component {
     });
   };
 
+  handleReset = () => {
+    console.log("Handle Reset");
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
